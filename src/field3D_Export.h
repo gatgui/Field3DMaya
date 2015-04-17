836ca09850ea14767c6136890ceb0e09048890e8
Gaetan Guidet <gatgui@gmail.com>
2015-04-18 00:14:56 +0900

    exportF3d command: revisited flags, support double type, use xml desc f3d.remap

--- src/field3D_Export.h
+++ src/field3D_Export.h
@@ -8,7 +8,9 @@
 #include <maya/MDagPath.h>
 #include <maya/MSelectionList.h>
 #include <map>
+#include <set>
 #include <string>
+#include "field3D_Tools.h"
 
 class exportF3d : public MPxCommand
 {
@@ -33,37 +35,38 @@ private:
   
 private:
 
-  MString        m_outputDir;
-  MString        m_outputName;
-  bool           m_verbose;
+  MString m_outputDir;
+  MString m_outputName;
+  bool m_verbose;
   MSelectionList m_slist;
-  int            m_start; //<-start of simulation
-  int            m_end; //<-end of simulation
-  bool           m_hasDensity; //<- export densiyt as well
-  bool           m_hasTemperature; //<- export temprature as well
-  bool           m_hasFuel; //<- export fuel as well
-  bool           m_hasColor; //<- export color as well
-  bool           m_hasVelocity; //<- export velocity as well
-  bool           m_hasPressure; //<- export presurre as well
-  bool           m_hasTexture; //<- export texture as well
-  bool           m_hasFalloff; //<- export falloff as well
-  int            m_numOversample; //<- oversamples the fluids but only writes out on whole frames
-  bool           m_forNCache; //<- export with .xml for use with nCache node
-  bool           m_ignoreDensity;
-  bool           m_ignoreTemperature;
-  bool           m_ignoreFuel;
-  bool           m_ignoreColor;
-  bool           m_ignoreVelocity;
-  bool           m_ignorePressure;
-  bool           m_ignoreTexture;
-  bool           m_ignoreFalloff;
-  bool           m_sparse;
-  bool           m_half;
-  double         m_sparseThreshold;
-  int            m_sparseBlockOrder;
-  double         m_sparseScalarDefault;
-  double         m_sparseVectorDefault[3];
+  int m_start; //<-start of simulation
+  int m_end; //<-end of simulation
+  bool m_hasDensity; //<- export densiyt as well
+  bool m_hasTemperature; //<- export temprature as well
+  bool m_hasFuel; //<- export fuel as well
+  bool m_hasColor; //<- export color as well
+  bool m_hasVelocity; //<- export velocity as well
+  bool m_hasPressure; //<- export presurre as well
+  bool m_hasTexture; //<- export texture as well
+  bool m_hasFalloff; //<- export falloff as well
+  int m_numOversample; //<- oversamples the fluids but only writes out on whole frames
+  bool m_xml;
+  bool m_ignoreDensity;
+  bool m_ignoreTemperature;
+  bool m_ignoreFuel;
+  bool m_ignoreColor;
+  bool m_ignoreVelocity;
+  bool m_ignorePressure;
+  bool m_ignoreTexture;
+  bool m_ignoreFalloff;
+  bool m_sparse;
+  Field3DTools::FieldDataTypeEnum m_format;
+  double m_sparseThreshold;
+  int m_sparseBlockOrder;
+  double m_sparseScalarDefault;
+  double m_sparseVectorDefault[3];
   std::map<std::string, std::string> m_remapChannels;
+  std::set<std::string> m_exportedChannels;
 };
 
 
